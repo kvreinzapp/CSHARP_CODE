5c68f3759df955c5dfde8ceb8e1845e8cbc672e0
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-10-24 22:22:25 +0800

    add MS_C# new project

--- HeadFirstC#/Projects/MyFirstConsoleApp/Program.cs
+++ HeadFirstC#/Projects/MyFirstConsoleApp/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+OperatorExamples();
+void OperatorExamples()
+{
+    int width = 3;
+    width++;
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+    while (area < 50)
+    {
+        height++;
+        area = width * height;
+    }
+
+    do
+    {
+        width--;
+        area = width * height;
+    } while (area > 25);
+
+    string result = "The area";
+    result = result + "is" + area;
+    Console.WriteLine(result);
+
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+}
---
+++ MicroSoftLearnCSharp/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] fraudulentOrderIDs = new string[3];
+Console.WriteLine("Hello, World!");
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
