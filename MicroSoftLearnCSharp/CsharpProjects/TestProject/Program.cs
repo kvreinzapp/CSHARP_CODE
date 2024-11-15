a728ee6a225d93c6ae701aedddb393745325ab33
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-11-15 14:45:49 +0800

    clear all unneccessary files

--- MicroSoftLearnCSharp/CsharpProjects/TestProject/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
-
-Console.WriteLine(largerValue);
