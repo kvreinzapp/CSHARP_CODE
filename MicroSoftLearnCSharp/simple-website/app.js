32c29b60af536106b0e9d41640eb4bdb34ac0601
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-12-09 18:41:20 +0800

    add change mode button, change both the theme and the text is amazing

---
+++ MicroSoftLearnCSharp/simple-website/app.js
@@ -0,0 +1,13 @@
+'use strict';
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function () {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+    let className = document.body.className;
+    if (className === "light-theme") {
+        switcher.textContent = "Dark";
+    } else {
+        switcher.textContent = "Light";
+    }
+    console.log(`current class name: ${className}`);
+});
